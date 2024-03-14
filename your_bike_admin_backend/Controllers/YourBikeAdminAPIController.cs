cc4a5e5f8a77e91445a934e66cdb94f2eae8d704
Neloy-SWE <taufiqneloy.swe@gmail.com>
2024-03-14 21:59:30 +0600

    log manager added

--- your_bike_admin_backend/Controllers/YourBikeAdminAPIController.cs
+++ your_bike_admin_backend/Controllers/YourBikeAdminAPIController.cs
@@ -4,7 +4,8 @@ namespace your_bike_admin_backend.Controllers
 {
     [Route("apiAdmin/youBike")]
     [ApiController]
-    public class YourBikeAdminAPIController
+    public class YourBikeAdminAPIController : ControllerBase
     {
+
     }
 }
