b134f9d118d46056ef0f5307c992936a4b247023
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-16 21:03:37 +0600

    notification model update

--- your_bike_admin_backend/Models/Notification.cs
+++ your_bike_admin_backend/Models/Notification.cs
@@ -5,8 +5,6 @@ namespace your_bike_admin_backend.Models
 {
     public class Notification
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public required string Brand { get; set; }
         public required string Model { get; set; }
